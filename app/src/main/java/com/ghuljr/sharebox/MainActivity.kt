b4c27c1cbcfdbf98912c6a06f72e425e0a74e723
GhulJr <51434437+GhulJr@users.noreply.github.com>
2023-06-27 21:29:47 +0200

    Build: project styling (#1)
    * Chore: add project theme
    
    * Chore: add drawables
    
    * Chore: update gitignore
    
    * Chore: add everything after restarting gitignore
    
    * Feat: add font
    
    * Feat: build typography
    
    * Build: udpate compose
    
    * Feat: adjust font weights
    
    * Chore: remove idea from git
    
    ---------
    
    Co-authored-by: Oskar Rek <oskar.rek@appunite.com>

--- app/src/main/java/com/ghuljr/sharebox/MainActivity.kt
+++ app/src/main/java/com/ghuljr/sharebox/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ghuljr.sharebox.ui.theme.ShareBoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShareBoxTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(
+                        modifier = Modifier.padding(top = 16.dp),
+                        name = "Android"
+                    )
                 }
             }
         }
@@ -33,7 +37,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.bodySmall
     )
 }
 
--- app/src/main/java/com/ghuljr/sharebox/ui/theme/Type.kt
+++ app/src/main/java/com/ghuljr/sharebox/ui/theme/Type.kt
@@ -2,33 +2,112 @@ package com.ghuljr.sharebox.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ghuljr.sharebox.R
+
+private val fontFamily = FontFamily(
+    Font(R.font.mplus_rounded_black, FontWeight.Black),
+    Font(R.font.mplus_rounded_bold, FontWeight.Bold),
+    Font(R.font.mplus_rounded_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.mplus_rounded_light, FontWeight.Light),
+    Font(R.font.mplus_rounded_medium, FontWeight.Medium),
+    Font(R.font.mplus_rounded_regular, FontWeight.Normal),
+    Font(R.font.mplus_rounded_thin, FontWeight.Thin)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        lineHeight = 64.sp,
+        fontSize = 57.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 11.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
 )
