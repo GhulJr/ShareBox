06c75de8634221664e2b894654ae239fab5c7bea
GhulJr <oskarrek98@gmail.com>
2023-09-11 21:46:35 +0200

    Feat: create app bar as component

---
+++ app/src/main/java/com/ghuljr/sharebox/ui/components/ShareBoxAppBar.kt
@@ -0,0 +1,43 @@
+package com.ghuljr.sharebox.ui.components
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ghuljr.sharebox.ui.theme.ShareBoxTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShareBoxAppBar(
+    modifier: Modifier = Modifier,
+    scrollBehaviour: TopAppBarScrollBehavior? = null,
+    title: String = ""
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        scrollBehavior = scrollBehaviour,
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)fun ShareBoxAppBarPreview() {
+    ShareBoxTheme {
+        ShareBoxAppBar(
+            title = "Hello ShareBox!"
+        )
+    }
+}
--- app/src/main/java/com/ghuljr/sharebox/ui/screens/NoteScreen.kt
+++ app/src/main/java/com/ghuljr/sharebox/ui/screens/NoteScreen.kt
@@ -1,35 +1,40 @@
 package com.ghuljr.sharebox.ui.screens
 
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import com.ghuljr.sharebox.ui.components.ShareBoxAppBar
 import com.ghuljr.sharebox.ui.theme.ShareBoxTheme
 import com.ghuljr.sharebox.ui.utils.loremIpsum
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreen() {
+    val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
         topBar = {
-            CenterAlignedTopAppBar(
-                scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
-                title = {
-                    Text(
-                        text = "ShareBox",
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                }
+            ShareBoxAppBar(
+                title = "ShareBox",
+                scrollBehaviour = scrollBehaviour
             )
         }
-    ) { Greeting(name = loremIpsum()) }
+    ) { contentPadding ->
+        Greeting(
+            modifier = Modifier.padding(contentPadding),
+            name = loremIpsum()
+        )
+    }
 }
 
 @Composable
@@ -47,9 +52,10 @@ fun NoteScreenPreview() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState(0)
     Text(
+        modifier = modifier.verticalScroll(scrollState),
         text = "Hello $name!",
-        modifier = modifier,
         style = MaterialTheme.typography.bodySmall
     )
 }
